946950a5f23e8dc9b50ffd0e78deed01b050b77d
jhony <putzzveio@gmail.com>
2024-10-10 17:34:32 -0300

    Primeiro_commit

---
+++ Controle_Individual.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+
+
+#st.set_page_config(layout="wide")
+tb_base = pd.read_csv('datasets\\controle de faltas.csv')
+
+
+#tb_base["DATA"] = pd.to_datetime(tb_base["DATA"], dayfirst=True, errors='coerce')
+
+substituicoes = {
+    "JHONY": "JHONY ALLISON",
+    "HUGO": "HUGO MANICARDI",
+    "RAFAELA": "RAFAELA LUPPI"
+}
+
+tb_base["NOME COLABORADOR"] = tb_base["NOME COLABORADOR"].replace(substituicoes)
+
+logo = "assets\\LOGO_MARIMEL.png"
+
+with st.container():
+    title = st.title("Controle de Faltas - :red[RH]")
+    st.divider()
+
+
+datamax = tb_base["DATA"].max()
+datamin = tb_base["DATA"].min()
+
+
+with st.sidebar:
+    st.logo(logo,size="large" )
+    Colab_select_box = st.selectbox("Colaboradores", tb_base["NOME COLABORADOR"].unique())
+    
+
+#Filtra_data = st.sidebar.slider("Data da Falta: ", datamin, datamax, datamax)
+
+df_select_box_filtrado = tb_base[tb_base["NOME COLABORADOR"] == Colab_select_box]
+
+KPI_faltas = df_select_box_filtrado.groupby("TIPO")["TEMPO(DIAS)"].sum().reset_index()
+
+title= st.title(f"Total de Faltas para {Colab_select_box}")
+
+num_kpis = len(KPI_faltas)
+col = st.columns(num_kpis)
+
+for index, row in KPI_faltas.iterrows():
+    col[index].metric(label=row["TIPO"], value=row["TEMPO(DIAS)"])
+
+st.subheader("Dados Filtrados: ")
+
+df_filtrado_ordenado = df_select_box_filtrado.sort_values(by="DATA", ascending=False)
+
+df_filtrado_ordenado
+
+
+        
---
+++ pages/Tabela.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout="centered")
+
+#st.set_page_config(layout="wide")
+tb_base = pd.read_csv('datasets\\controle de faltas.csv')
+tb_base["DATA"] = pd.to_datetime(tb_base["DATA"], dayfirst=True, errors='coerce')
+
+tb_base_new = tb_base.sort_values(by="DATA", ascending=False)
+
+logo = "assets\\LOGO_MARIMEL.png"
+
+with st.container():
+    title = st.title("Controle de Faltas - :red[RH]")
+    st.divider()
+
+with st.sidebar:
+    st.logo(logo,size="large" )
+    #Colab_select_box = st.selectbox("Colaboradores", tb_base["NOME COLABORADOR"].unique())
+    
+
+
+
+tb_base_new
